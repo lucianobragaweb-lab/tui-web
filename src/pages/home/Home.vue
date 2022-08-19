599bfca9886d488fec1639420a1065734b8aa9a1
lucianobragaweb <lucianobragaweb@gmail.com>
2022-08-19 17:42:24 -0300

    move project to root folder

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,7 +2,6 @@
   <div class="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-12">
     <section aria-labelledby="timeline-title" class="lg:col-span-3">
       <CountryFilter />
-      <button @click="increment">OK {{ count() }}</button>
     </section>
 
     <div class="space-y-2 lg:col-span-9">
@@ -15,7 +14,6 @@
 import { defineComponent, ref, computed } from 'vue'
 import CountryFilter from '../../components/CountryFilter.vue' // @ is an alias to /src
 import HotelsList from '../../components/HotelsList.vue' // @ is an alias to /src
-import Product from '../../types/Product'
 import products from '../../api/products.json'
 import { useStore } from 'vuex'
 
@@ -29,16 +27,7 @@ export default defineComponent({
     const store = useStore()
     const hotels = ref(products)
 
-    const count = () => computed(() => store.state.counter)
-
-    console.log(store.state.counter)
-
-    const increment = () => store.commit('increment')
-
-    return { hotels, increment, count }
-  },
-  mounted () {
-    console.log('aqui')
+    return { hotels }
   }
 })
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,10 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    counter: 0
   },
   getters: {
   },
   mutations: {
-    increment (state) {
-      state.counter++
-    }
   },
   actions: {
   },
